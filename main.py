b327f6eda86f8bbf92519b858e5cfc632512da50
NatanaelJose <josesaraivamaciel@gmail.com>
2023-08-13 12:22:31 -0300

    array of videos

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import youtube_dl
 import os
 
-# URL do vídeo do YouTube
-video_url = 'https://youtu.be/MURua52_YPg'
+# URL dos vídeos do YouTube
+videos_urls = ['url do video', ' url do outro video']  # url dos videos
 
 # Pasta de destino para download
 output_path = './downloads/'
 
 
 def download_audio(url):
+    print("Downloading:", url)
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -22,7 +23,7 @@ def download_audio(url):
         info_dict = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info_dict)
         new_filename = os.path.splitext(filename)[0] + '.mp3'
-        os.rename(filename, new_filename)
 
 
-download_audio(video_url)
+for video_url in videos_urls:
+    download_audio(video_url)
